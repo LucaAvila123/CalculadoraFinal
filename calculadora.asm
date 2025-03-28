0908fc32d05d9c489c77215136a1f475e2671e2d
Luca <91071096+LucaAvila123@users.noreply.github.com>
2025-03-28 16:59:39 -0300

    Create calculadora.asm

---
+++ calculadora.asm
@@ -0,0 +1,34 @@
+; Atividade Final
+; Alunos: Elder, Luca e Pedro
+
+segment code
+..start:
+    
+main:
+    mov byte[negativo1], '-' ; Move AL para [negativo1]
+    mov bl, [negativo1]
+    cmp bl, '+'
+    je imprimir
+
+    call sair
+sair:
+    ; Resultado da função de sair sendo chamada
+    mov    ax, 4C00h
+    int     21h
+
+imprimir:
+    ; Imprime o valor de negativo1
+    mov ah, 0Eh
+    mov al, '+'
+    mov bl, 07h
+    int 10h
+    call sair
+
+segment .data
+    num1 dw 0
+    num2 dw 0
+    op   db 0  ; Operador (+, -, *, /)
+    digito1 db 0
+    digito2 db 0
+    negativo1 db '+',0 ; Operador primeiro número (-)
+    negativo2 db 43,0 ; Operador segundo número
